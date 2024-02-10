0f27aed3cb3e1d08a92385ee4d0b2b626082269a
akshraj chavda <90257725+akshraj-05@users.noreply.github.com>
2024-02-10 20:51:05 +0530

    node project working

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
-const { ping } = require('./ping/ping'); // Assuming you have a ping module
+const pingWebsite = require('./ping/ping'); // Assuming you have a ping module
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve static files (CSS, JS, etc.)
 app.use('/static', express.static(path.join(__dirname, 'static')));
@@ -13,12 +15,22 @@ app.get('/', (req, res) => {
 });
 
 // Handle form submission
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     const domain = req.body.domain;
     console.log(domain);
 
-   // ping(domain)
-   
+    try {
+        let obj = await pingWebsite(domain);
+        res.send(
+            `The latency of domain: ${domain} is ${obj.latency.toString()} ms`  
+        );
+    } catch (error) {
+     
+        res.status(404).send({
+            success: false,
+            message: error.message
+        });
+    }
 });
 
 // Start the server
